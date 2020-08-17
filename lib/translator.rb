872922da1d5766ef37d8669592a4a5621065ea71
Wesley Drake Faircloth <wesfairclothh@gmail.com>
2020-08-17 22:00:12 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -24,5 +24,7 @@ def get_japanese_emoticon(emoticon_file, emoticon)
 end
 
 def get_english_meaning
-  # code goes here
+ emoticon_lib = load_library(emoticon_file)
+   english_meaning = emoticon_lib['get_meaning'][emoticon]
+   english_meaning ? english_meaning : 'Sorry, that emoticon was not found'
 end
