56c7b7377f5eb40f45b51d4fa1c1e8bcfa06c126
Wesley Drake Faircloth <wesfairclothh@gmail.com>
2020-08-17 21:59:46 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,7 +18,9 @@ emoticons = YAML.load_file('./lib/emoticons.yml')
 end
 
 def get_japanese_emoticon(emoticon_file, emoticon)
-  # code goes here
+ emoticon_lib = load_library(emoticon_file)
+   japanese_emoticon = emoticon_lib['get_emoticon'][emoticon]
+   japanese_emoticon ? japanese_emoticon : 'Sorry, that emoticon was not found'
 end
 
 def get_english_meaning
